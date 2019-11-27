14616d82639dfc16b94c5df5da561e92e00dfd21
DavidLineman <dline215@gmail.com>
2019-11-27 14:00:41 -0500

    added overall styling to app.html

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :description, length: { minimum: 10 }, :message => "Product description has to be at least 10 characters."
 end
