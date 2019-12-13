f1788dffc668520f34218d283f51598a59f0aab4
DavidLineman <dline215@gmail.com>
2019-12-13 16:35:14 -0500

    added test for ajax cart function

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -49,4 +49,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
+
+  test "should create line_item via ajax" do 
+    assert_difference(LineItem.count) do 
+      post line_items_url, param: { product_id: products(:ruby).id },
+        xhr: true
+    end
+
+    assert_response :success
+    assert_match /<tr class=\\ "line-item-higlight"/, @response.body
+  end
 end
